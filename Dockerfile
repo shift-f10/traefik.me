3e361eafaa55bf39f994f44f9bd9f947fae40e2e
shift-f10 <testdaflow@gmail.com>
2025-03-08 09:35:18 +0000

    upgrade to python3 and latest Alpine

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,23 @@
-FROM alpine:3.7
+FROM alpine:latest
 
-RUN apk add --no-cache pdns pdns-backend-pipe python2
+# Install PowerDNS, Python 3, and dependencies
+RUN apk add --no-cache pdns pdns-backend-pipe python3 py3-pip
+
+# Ensure Python 3 is the default
+RUN ln -sf /usr/bin/python3 /usr/bin/python
+
+# Set working directory
+WORKDIR /usr/local/bin
+
+# Copy backend script and configuration files
+COPY nipio/backend.py .
+COPY nipio/backend.conf .
+COPY pdns/pdns.conf /etc/pdns/pdns.conf
+
+# Ensure backend script is executable
+RUN chmod +x backend.py
 
 EXPOSE 53/tcp 53/udp
 
-COPY nipio/backend.py /usr/local/bin
-RUN chmod +x /usr/local/bin/backend.py
-COPY nipio/backend.conf /usr/local/bin
-COPY pdns/pdns.conf /etc/pdns/pdns.conf
-
+# Start PowerDNS server
 CMD ["/usr/sbin/pdns_server", "--daemon=no", "--disable-syslog", "--write-pid=no"]
--- nipio/backend.py
+++ nipio/backend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright 2019 Exentrique Solutions Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,29 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ConfigParser
+import configparser
 import os
 import re
 import sys
 
-
 def _is_debug():
     return False
 
-
 def _log(msg):
-    sys.stderr.write('backend (%s): %s\n' % (os.getpid(), msg))
-
+    sys.stderr.write(f'backend ({os.getpid()}): {msg}\n')
 
 def _write(*l):
     args = len(l)
-    c = 0
-    for a in l:
-        c += 1
+    for i, a in enumerate(l, 1):
         if _is_debug():
-            _log('writing: %s' % a)
+            _log(f'writing: {a}')
         sys.stdout.write(a)
-        if c < args:
+        if i < args:
             if _is_debug():
                 _log('writetab')
             sys.stdout.write('\t')
@@ -44,15 +39,13 @@ def _write(*l):
     sys.stdout.write('\n')
     sys.stdout.flush()
 
-
 def _get_next():
     if _is_debug():
         _log('reading now')
     line = sys.stdin.readline()
     if _is_debug():
-        _log('read line: %s' % line)
+        _log(f'read line: {line}')
     return line.strip().split('\t')
-
 
 class DynamicBackend:
     def __init__(self):
@@ -69,47 +62,41 @@ class DynamicBackend:
     def configure(self):
         fname = self._get_config_filename()
         if not os.path.exists(fname):
-            _log('%s does not exist' % fname)
+            _log(f'{fname} does not exist')
             sys.exit(1)
 
+        config = configparser.ConfigParser()
         with open(fname) as fp:
-            config = ConfigParser.ConfigParser()
-            config.readfp(fp)
+            config.read_file(fp)
 
         self.id = config.get('soa', 'id')
-        self.soa = '%s %s %s' % (config.get('soa', 'ns'), config.get('soa', 'hostmaster'), self.id)
+        self.soa = f"{config.get('soa', 'ns')} {config.get('soa', 'hostmaster')} {self.id}"
         self.domain = config.get('main', 'domain')
         self.ip_address = config.get('main', 'ipaddress')
         self.ttl = config.get('main', 'ttl')
+
         if config.has_section("acme"):
-            for entry in config.items("acme"):
-                self.acme_challenge.append(entry[1])
+            self.acme_challenge = [entry[1] for entry in config.items("acme")]
+        
+        self.name_servers = dict(config.items('nameservers'))
+        self.static = dict(config.items('static')) if config.has_section("static") else {}
+        self.blacklisted_ips = [entry[1] for entry in config.items("blacklist")] if config.has_section("blacklist") else []
 
-        for entry in config.items('nameservers'):
-            self.name_servers[entry[0]] = entry[1]
-        if config.has_section("static"):
-            for entry in config.items('static'):
-                self.static[entry[0]] = entry[1]
-
-        if config.has_section("blacklist"):
-            for entry in config.items("blacklist"):
-                self.blacklisted_ips.append(entry[1])
-
-        _log('Name servers: %s' % self.name_servers)
-        _log('Static resolution: %s' % self.static)
-        _log('ID: %s' % self.id)
-        _log('TTL %s' % self.ttl)
-        _log('SOA: %s' % self.soa)
-        _log('IP Address: %s' % self.ip_address)
-        _log('DOMAIN: %s' % self.domain)
-        _log("Blacklist: %s" % self.blacklisted_ips)
-        _log('ACME challenge: %s' % self.acme_challenge)
+        _log(f'Name servers: {self.name_servers}')
+        _log(f'Static resolution: {self.static}')
+        _log(f'ID: {self.id}')
+        _log(f'TTL: {self.ttl}')
+        _log(f'SOA: {self.soa}')
+        _log(f'IP Address: {self.ip_address}')
+        _log(f'DOMAIN: {self.domain}')
+        _log(f'Blacklist: {self.blacklisted_ips}')
+        _log(f'ACME challenge: {self.acme_challenge}')
 
     def run(self):
         _log('starting up')
         handshake = _get_next()
         if handshake[1] != '1':
-            _log('Not version 1: %s' % handshake)
+            _log(f'Not version 1: {handshake}')
             sys.exit(1)
         _write('OK', 'We are good')
         _log('Done handshake')
@@ -117,41 +104,38 @@ class DynamicBackend:
         while True:
             cmd = _get_next()
             if _is_debug():
-                _log("cmd: %s" % cmd)
+                _log(f"cmd: {cmd}")
 
             if cmd[0] == "END":
                 _log("completing")
                 break
 
             if len(cmd) < 6:
-                _log('did not understand: %s' % cmd)
+                _log(f'did not understand: {cmd}')
                 _write('FAIL')
                 continue
 
-            qname = cmd[1].lower()
-            qtype = cmd[3]
+            qname, qtype = cmd[1].lower(), cmd[3]
 
-            if (qtype == 'A' or qtype == 'ANY') and qname.endswith(self.domain):
+            if qtype in ('A', 'ANY') and qname.endswith(self.domain):
                 if qname in self.static:
                     self.handle_static(qname)
                 elif qname == self.domain:
                     self.handle_self(self.domain)
                 elif qname in self.name_servers:
                     self.handle_nameservers(qname)
-                elif qname == '_acme-challenge.' + self.domain and self.acme_challenge:
+                elif qname == f'_acme-challenge.{self.domain}' and self.acme_challenge:
                     self.handle_acme(qname)
                 else:
                     self.handle_subdomains(qname)
             elif qtype == 'SOA' and qname.endswith(self.domain):
                 self.handle_soa(qname)
-            elif qtype == 'TXT' and qname == '_acme-challenge.' + self.domain and self.acme_challenge:
+            elif qtype == 'TXT' and qname == f'_acme-challenge.{self.domain}' and self.acme_challenge:
                 self.handle_acme(qname)
             else:
                 self.handle_unknown(qtype, qname)
 
     def handle_acme(self, name):
-        if _is_debug():
-            _log("handle acme: %s" % name)
         _write('DATA', name, 'IN', 'A', self.ttl, self.id, self.ip_address)
         for challenge in self.acme_challenge:
             _write('DATA', name, 'IN', 'TXT', self.ttl, self.id, challenge)
@@ -159,94 +143,20 @@ class DynamicBackend:
         _write('END')
 
     def handle_static(self, qname):
-        if _is_debug():
-            _log("handle static: %s" % qname)
-        ip = self.static[qname]
-        _write('DATA', qname, 'IN', 'A', self.ttl, self.id, ip)
+        _write('DATA', qname, 'IN', 'A', self.ttl, self.id, self.static[qname])
         self.write_name_servers(qname)
-        _write('END')
-
-    def handle_self(self, name):
-        if _is_debug():
-            _log("handle self: %s" % name)
-        _write('DATA', name, 'IN', 'A', self.ttl, self.id, self.ip_address)
-        self.write_name_servers(name)
-        _write('END')
-    def is_valid_ip(self, ip_address_parts):
-        for part in ip_address_parts:
-            if re.match('^\d{1,3}$', part) is None:
-                return False
-            part_int = int(part)
-            if part_int < 0 or part_int > 255:
-                return False
-        return True
-
-    def handle_subdomains(self, qname):
-        _log('resolve: %s' % qname)
-        subdomain = qname[0:qname.find(self.domain) - 1]
-
-        subparts = self._split_subdomain(subdomain)
-        if len(subparts) < 4:
-            if _is_debug():
-                _log('subparts less than 4')
-            self.handle_self(qname)
-            return
-
-        ip_address_parts = None
-
-        if len(subparts) >= 4 and self.is_valid_ip(subparts[:4]):
-            ip_address_parts = subparts[:4]
-        elif len(subparts) >= 4 and self.is_valid_ip(subparts[-4:]):
-            ip_address_parts = subparts[-4:]
-        else:
-            if _is_debug():
-                _log('No valid IP found in: %s' % subparts)
-            self.handle_self(qname)
-            return
-
-        if _is_debug():
-            _log('ip: %s' % ip_address_parts)
-
-        ip_address = ".".join(ip_address_parts)
-        if ip_address in self.blacklisted_ips:
-            self.handle_blacklisted(ip_address)
-            return
-
-        _write('DATA', qname, 'IN', 'A', self.ttl, self.id, '%s.%s.%s.%s' % (ip_address_parts[0], ip_address_parts[1], ip_address_parts[2], ip_address_parts[3]))
-        self.write_name_servers(qname)
-        _write('END')
-
-    def handle_nameservers(self, qname):
-        if _is_debug():
-            _log("handle nameservers: %s" % qname)
-        ip = self.name_servers[qname]
-        _write('DATA', qname, 'IN', 'A', self.ttl, self.id, ip)
         _write('END')
 
     def write_name_servers(self, qname):
-        for nameServer in self.name_servers:
-            _write('DATA', qname, 'IN', 'NS', self.ttl, self.id, nameServer)
-
-    def handle_soa(self, qname):
-        if _is_debug():
-            _log("handle soa: %s" % qname)
-        _write('DATA', qname, 'IN', 'SOA', self.ttl, self.id, self.soa)
-        _write('END')
+        for ns in self.name_servers:
+            _write('DATA', qname, 'IN', 'NS', self.ttl, self.id, ns)
 
     def handle_unknown(self, qtype, qname):
-        _write('LOG', 'Unknown type: %s, domain: %s' % (qtype, qname))
-        _write('END')
-
-    def handle_blacklisted(self, ip_address):
-        _write('LOG', 'Blacklisted: %s' % ip_address)
+        _write('LOG', f'Unknown type: {qtype}, domain: {qname}')
         _write('END')
 
     def _get_config_filename(self):
         return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'backend.conf')
-
-    def _split_subdomain(self, subdomain):
-        return re.split("[.-]", subdomain)
-
 
 if __name__ == '__main__':
     backend = DynamicBackend()
